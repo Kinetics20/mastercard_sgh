852f0ea1cc212601b95d829dbb504e50e44d9a4a
Kinetics20 <kinetics20@gmail.com>
2025-05-31 16:42:17 +0200

    add numerical and categorical data function, analysis transactions.parquet data

--- mastercard/__init__.py
+++ mastercard/__init__.py
@@ -1,1 +1,3 @@
-from mastercard import config  # noqa: F401
+from mastercard import config
+from mastercard.visualisations.numerical_data import dist_visualisation
+from mastercard.visualisations.categorical_data import dist_categorical_visualisation
